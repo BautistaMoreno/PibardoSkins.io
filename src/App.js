34b8a53cf6f7802d0ef0e4314c3c4f62479dd28a
Lea <mileabi05@gmail.com>
2024-10-24 15:15:23 -0300

    deploy react y route app js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import ThemeProvider from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import Terms from "./pages/Terms";
 import Contac from "./pages/Contac";
-import Sidebar from "./components/Sidebar"; 
-import './App.css'; 
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <ThemeProvider>
         <div className="app-container">
-          <Sidebar /> 
+          <Sidebar />
           <div className="content">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/PibardoSkins.io/" element={<Home />} />
               <Route path="/terminos" element={<Terms />} />
               <Route path="/contacto" element={<Contac />} />
             </Routes>
